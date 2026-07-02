55cca2f0b0b6f75737ab96fff8e0e0e94de15705
H1AV <ariasvespa@gmail.com>
2026-07-02 16:07:38 -0400

    Add registrarKardex function to KardexRepository

--- PROYECTO.FARMACIAV2/farmacia-salud-total/backend/repositories/KardexRepository.js
+++ PROYECTO.FARMACIAV2/farmacia-salud-total/backend/repositories/KardexRepository.js
@@ -1,1 +1,5 @@
+import KardexControl from '../models/KardexControl.js';
 
+export const registrarKardex = async (datosKardex, transaccion) => {
+  return await KardexControl.create(datosKardex, { transaction: transaccion });
+};
